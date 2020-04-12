d93ce7fb58d632a868d141c1a2b0bcb4587c366d
David Von Derau <dvonderau@cobaltblue.us>
2020-04-11 19:00:50 -0700

    Use Harmony to patch EncyclopediaFactionPageVM.
    The patched method returns our type EncyclopediaFactionImprovedVM, an extension of EncyclopediaFactionVM with a property for `IsClanDestroyed`. This new property is used by our version of EncyclopediaClanSubPageElement.xml to grey out the banner of destroyed factions.

---
+++ DeadClansDeactivate/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DeadClansDeactivate")]
+[assembly: AssemblyDescription("A mod for Mount & Blade 2: Bannerlord.")]
+[assembly: ComVisible(false)]
+[assembly: Guid("78da943f-955b-4823-acab-1344c176e445")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DeadClansDeactivate/SubModule.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Library;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia.EncyclopediaItems;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia;
+
+namespace DeadClansDeactivate
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            var harmony = new Harmony($"mod.bannerlord.dvd.{nameof(DeadClansDeactivate)}");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(EncyclopediaFactionPageVM), nameof(EncyclopediaFactionPageVM.Refresh))]
+    class EncyclopediaFactionPageVMPatch
+    {
+        // ReSharper disable once InconsistentNaming
+        private static readonly FieldInfo _faction;
+
+        static EncyclopediaFactionPageVMPatch()
+        {
+            _faction = typeof(EncyclopediaFactionVM).GetFields(BindingFlags.Instance | BindingFlags.NonPublic).Single(fieldInfo => fieldInfo.Name == nameof(_faction));
+        }
+
+        // ReSharper disable once InconsistentNaming
+        static void Postfix(EncyclopediaFactionPageVM __instance)
+        {
+            __instance.IsLoadingOver = false;
+            for (var idx = 0; idx < __instance.Clans.Count; ++idx)
+            {
+                var original = __instance.Clans[idx];
+                __instance.Clans[idx] = new EncyclopediaFactionImprovedVM((IFaction)_faction.GetValue(original));
+            }
+            __instance.IsLoadingOver = true;
+        }
+    }
+
+    public sealed class EncyclopediaFactionImprovedVM : EncyclopediaFactionVM
+    {
+        [DataSourceProperty]
+        public bool IsClanDestroyed
+        {
+            get => _isClanDestroyed;
+            set
+            {
+                if (value == _isClanDestroyed)
+                    return;
+                _isClanDestroyed = value;
+                OnPropertyChanged(nameof(IsClanDestroyed));
+            }
+        }
+
+        private bool _isClanDestroyed;
+
+        private readonly IFaction _faction;
+
+        public EncyclopediaFactionImprovedVM(IFaction faction)
+            : base(faction)
+        {
+            _faction = faction;
+            RefreshValues();
+        }
+
+        public override void RefreshValues()
+        {
+            base.RefreshValues();
+            if (_faction == null)
+            {
+                return;
+            }
+
+            IsClanDestroyed = _faction.IsDeactivated;
+        }
+    }
+}
